cb3f9e9266230be832f8e8dd8028b08952e8c57c
Yusuf Mohamed <yusuf.mohamed.work@gmail.com>
2025-10-31 20:44:44 +0300

    headache inducing refactoring, struggled with handling env vars and other stuff

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -10,21 +10,39 @@ import (
 
 	"github.com/Yusufdot101/note-nest/internal/custom_errors"
 	"github.com/Yusufdot101/note-nest/internal/user"
+	"github.com/joho/godotenv"
 	"github.com/julienschmidt/httprouter"
 	_ "github.com/lib/pq"
 )
 
 const PORT = ":8080"
 
+var dsn string
+
+func init() {
+	if os.Getenv("APP_ENV") != "docker" {
+		if err := godotenv.Load(); err != nil {
+			log.Panicf("could not load .env file: %v", err)
+		}
+		dsn = os.Getenv("LOCAL_DB_URL")
+	} else {
+		dsn = os.Getenv("DOCKER_DB_URL")
+	}
+
+	if dsn == "" {
+		log.Panic("DB_URL is not set")
+	}
+}
+
 func main() {
-	dsn := os.Getenv("DSN")
-	router := httprouter.New()
-	router.NotFound = http.HandlerFunc(custom_errors.NotFoundErrorResponse)
-	router.MethodNotAllowed = http.HandlerFunc(custom_errors.MethodNotAllowedErrorResponse)
 	DB, err := openDB(dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(custom_errors.NotFoundErrorResponse)
+	router.MethodNotAllowed = http.HandlerFunc(custom_errors.MethodNotAllowedErrorResponse)
 	user.RegisterRoutes(router, DB)
 
 	log.Printf("server running on %s\n", PORT)
