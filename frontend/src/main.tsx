0107dfe9d6a534deac23f638e833251889a9339f
Yusuf Mohamed <yusuf.mohamed.work@gmail.com>
2025-10-31 07:50:03 +0300

    NN-62: fixed the inputs and used Outlet to get Header and Footer inside RouterProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,21 +4,20 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import Signup from './components/Signup.tsx'
-import Header from './components/Header.tsx'
-import Footer from './components/Footer.tsx'
+import Layout from './components/Layout.tsx'
 
 const router = createBrowserRouter([
-    { path: "/", element: <App /> },
-    { path: "/signup", element: <Signup /> },
+    {
+        element: <Layout />,
+        children: [
+            { path: "/", element: <App /> },
+            { path: "/signup", element: <Signup /> },
+        ]
+    }
 ])
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <div className="bg-background min-h-screen flex flex-col p-[32px] gap-[32px]">
-            <Header />
-            <RouterProvider router={router} >
-            </RouterProvider>
-            <Footer />
-        </div>
+        <RouterProvider router={router} />
     </StrictMode>,
 )
