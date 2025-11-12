5fa5ee5f64ee386308f7e716f99876fbdc87ff12
Yusuf Mohamed <yusuf.mohamed.work@gmail.com>
2025-11-12 15:23:24 +0300

    fixed project/get_handlers.go

--- backend/internal/project/get_handler.go
+++ backend/internal/project/get_handler.go
@@ -8,28 +8,12 @@ import (
 	"github.com/Yusufdot101/note-nest/internal/custom_errors"
 	"github.com/Yusufdot101/note-nest/internal/middleware"
 	"github.com/Yusufdot101/note-nest/internal/utilities"
-<<<<<<< HEAD
-)
-
-func (h *ProjectHandler) GetProjects(w http.ResponseWriter, r *http.Request) {
-	currentUserID, ok := r.Context().Value(middleware.CtxUserIDKey).(int)
-	if !ok {
-		custom_errors.ServerErrorResponse(w, errors.New("userID missing from context"))
-		return
-	}
-
-	var userID int
-
-	queryUserID := r.URL.Query().Get("user")
-	queryProjectID := r.URL.Query().Get("project")
-=======
 	"github.com/julienschmidt/httprouter"
 )
 
 func (h *ProjectHandler) GetProjects(w http.ResponseWriter, r *http.Request) {
 	var userID int
 	queryUserID := r.URL.Query().Get("user")
->>>>>>> 9946a38 (port monolithic handlers.go into refactored files)
 	visibility := r.URL.Query().Get("visibility")
 
 	if queryUserID != "" {
@@ -41,46 +25,14 @@ func (h *ProjectHandler) GetProjects(w http.ResponseWriter, r *http.Request) {
 		}
 		visibility = "public"
 	} else {
-<<<<<<< HEAD
-		userID = currentUserID
-	}
-
-	var result any
-	var err error
-	if queryProjectID != "" {
-		projectID, err := strconv.Atoi(queryProjectID)
-		if err != nil {
-			custom_errors.BadRequestErrorResponse(w, err)
-			return
-		}
-
-		result, err = h.svc.GetProject(currentUserID, projectID)
-		if err != nil {
-			switch {
-			case errors.Is(err, custom_errors.ErrNoRecord):
-				custom_errors.NotFoundErrorResponse(w, r)
-			default:
-				custom_errors.ServerErrorResponse(w, err)
-			}
-			return
-		}
-	} else {
-		result, err = h.svc.getProjects(userID, visibility)
-		if err != nil {
-			custom_errors.BadRequestErrorResponse(w, err)
-=======
 		var ok bool
 		userID, ok = r.Context().Value(middleware.CtxUserIDKey).(int)
 		if !ok {
 			custom_errors.ServerErrorResponse(w, errors.New("userID missing from context"))
->>>>>>> 9946a38 (port monolithic handlers.go into refactored files)
 			return
 		}
 	}
 
-<<<<<<< HEAD
-	err = utilities.WriteJSON(w, utilities.Message{"result": result}, http.StatusOK)
-=======
 	projects, err := h.svc.getProjects(userID, visibility)
 	if err != nil {
 		switch {
@@ -113,7 +65,7 @@ func (h *ProjectHandler) GetProject(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	project, err := h.svc.getProject(userID, projectID)
+	project, err := h.svc.GetProject(userID, projectID)
 	if err != nil {
 		switch {
 		case errors.Is(err, custom_errors.ErrNoRecord):
@@ -125,7 +77,6 @@ func (h *ProjectHandler) GetProject(w http.ResponseWriter, r *http.Request) {
 	}
 
 	err = utilities.WriteJSON(w, utilities.Message{"project": project}, http.StatusOK)
->>>>>>> 9946a38 (port monolithic handlers.go into refactored files)
 	if err != nil {
 		custom_errors.ServerErrorResponse(w, err)
 		return
