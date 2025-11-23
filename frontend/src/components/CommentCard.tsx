da8cb45ffe62becc68fbfd2ea260b8632f0fa336
Yusuf Mohamed <yusuf.mohamed.work@gmail.com>
2025-11-23 16:40:04 +0300

    fixed failing action

--- frontend/src/components/CommentCard.tsx
+++ frontend/src/components/CommentCard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ColorPicker from "./ColorPicker";
 import type { Comment } from "../utilities/comments";
 import { useAuthStore } from "../store/useAuthStore";
