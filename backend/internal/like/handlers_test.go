b2859ea43be18346d1ba517e647b278f204e07d8
Yusuf Mohamed <yusuf.mohamed.work@gmail.com>
2025-11-25 12:27:57 +0300

    wrote unit tests for the like package

---
+++ backend/internal/like/handlers_test.go
@@ -0,0 +1,104 @@
+package like
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Yusufdot101/note-nest/internal/middleware"
+	"github.com/julienschmidt/httprouter"
+)
+
+func TestAddLikeHandler(t *testing.T) {
+	userID := 1
+	noteID := "1"
+
+	wantStatusCode := http.StatusCreated
+
+	req := httptest.NewRequest(http.MethodPost, "/notes/:id/like", nil)
+	ctx := context.WithValue(req.Context(), middleware.CtxUserIDKey, userID)
+	params := httprouter.Params{httprouter.Param{Key: "id", Value: noteID}}
+	ctx = context.WithValue(ctx, httprouter.ParamsKey, params)
+	req = req.WithContext(ctx)
+
+	recorder := httptest.NewRecorder()
+
+	repo := &mockRepo{}
+	h := newHandler(&likeService{
+		repo: repo,
+	})
+
+	h.addLike(recorder, req)
+	res := recorder.Result()
+
+	if statusCode := res.StatusCode; statusCode != wantStatusCode {
+		t.Errorf("expected status code = %d, got status code = %d", wantStatusCode, statusCode)
+	}
+
+	if !repo.insertCalled {
+		t.Fatalf("expected repo.insert to be called")
+	}
+}
+
+func TestRemoveLikeHandler(t *testing.T) {
+	userID := 1
+	noteID := "1"
+
+	wantStatusCode := http.StatusOK
+
+	req := httptest.NewRequest(http.MethodPost, "/notes/:id/like", nil)
+	ctx := context.WithValue(req.Context(), middleware.CtxUserIDKey, userID)
+	params := httprouter.Params{httprouter.Param{Key: "id", Value: noteID}}
+	ctx = context.WithValue(ctx, httprouter.ParamsKey, params)
+	req = req.WithContext(ctx)
+
+	recorder := httptest.NewRecorder()
+
+	repo := &mockRepo{}
+	h := newHandler(&likeService{
+		repo: repo,
+	})
+
+	h.removeLike(recorder, req)
+	res := recorder.Result()
+
+	if statusCode := res.StatusCode; statusCode != wantStatusCode {
+		t.Errorf("expected status code = %d, got status code = %d", wantStatusCode, statusCode)
+	}
+
+	if !repo.deleteCalled {
+		t.Fatalf("expected repo.delete to be called")
+	}
+}
+
+func TestNoteIsLikedHandler(t *testing.T) {
+	userID := 1
+	noteID := "1"
+
+	wantStatusCode := http.StatusOK
+
+	req := httptest.NewRequest(http.MethodPost, "/notes/:id/like", nil)
+	ctx := context.WithValue(req.Context(), middleware.CtxUserIDKey, userID)
+	params := httprouter.Params{httprouter.Param{Key: "id", Value: noteID}}
+	ctx = context.WithValue(ctx, httprouter.ParamsKey, params)
+	req = req.WithContext(ctx)
+
+	recorder := httptest.NewRecorder()
+
+	repo := &mockRepo{}
+	h := newHandler(&likeService{
+		repo: repo,
+	})
+
+	h.noteIsLiked(recorder, req)
+	res := recorder.Result()
+
+	if statusCode := res.StatusCode; statusCode != wantStatusCode {
+		t.Errorf("expected status code = %d, got status code = %d", wantStatusCode, statusCode)
+	}
+
+	if !repo.isLikedCalled {
+		t.Fatalf("expected repo.isLiked to be called")
+	}
+}
